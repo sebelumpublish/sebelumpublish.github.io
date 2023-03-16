2568b271aa46037adf0d6e74f86db90fce6527d1
sebelumpublish <cakermichael@gmail.com>
2023-03-16 10:44:29 +0800

    f

--- js/script.js
+++ js/script.js
@@ -1,58 +1,58 @@
-// var swiper = new Swiper(".slide-content", {
-//   slidesPerView: 3,
-//   spaceBetween: 25,
-//   loop: true,
-//   centerSlide: "true",
-//   fade: "true",
-//   grabCursor: "true",
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//     dynamicBullets: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-//   breakpoints: {
-//     0: {
-//       slidesPerView: 1,
-//     },
-//     520: {
-//       slidesPerView: 2,
-//     },
-//     950: {
-//       slidesPerView: 3,
-//     },
-//   },
-// });
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
 
-// function ScrollKeatasUp() {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// }
+function ScrollKeatasUp() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
 
-// const toTop = document.querySelector(".to-top");
+const toTop = document.querySelector(".to-top");
 
-// // scroll icon up
-// window.addEventListener("scroll", () => {
-//   if (window.pageYOffset > 200) {
-//     toTop.classList.add("active");
-//   } else {
-//     toTop.classList.remove("active");
-//   }
-// });
+// scroll icon up
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 200) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 
-// // mengecilkan logo
-// const logo = document.querySelector(".logo");
+// mengecilkan logo
+const logo = document.querySelector(".logo");
 
-// window.addEventListener("scroll", () => {
-//   if (window.pageYOffset > 90) {
-//     logo.classList.add("active");
-//   } else {
-//     logo.classList.remove("active");
-//   }
-// });
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 90) {
+    logo.classList.add("active");
+  } else {
+    logo.classList.remove("active");
+  }
+});
